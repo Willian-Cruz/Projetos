0feac98cfe4b2afbbb8ea75d5e86802f899b1afa
willian.adolpho95@gmail.com <willian.adolpho95@gmail.com>
2026-03-25 20:12:04 -0300

    "Correções no arquivo de extração dos dados"

--- scripts/extract.py
+++ scripts/extract.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 def extract():
     dados = [{"titulo":"Analista de Dados", "empresa":"Empresa A", "localidade":"São Paulo","salario":5000},
-             {"titulo":"Analista de Dados", "empresa":"Empresa A", "localidade":"São Paulo","salario":5000},
-             {"titulo":"Analista de Dados", "empresa":"Empresa A", "localidade":"São Paulo","salario":5000},
+             {"titulo":"Engenheiro de Dados", "empresa":"Empresa B", "localidade":"Rio de Janeiro","salario":8000},
+             {"titulo":"Cientista de Dados", "empresa":"Empresa C", "localidade": None,"salario":9000},
             ]
     df = pd.DataFrame(dados)
 
-    df.csv("data/raw/vagas.csv", index=False)
+    df.to_csv("../data/raw_data/vagas.csv", index=False)
     print("Dados extraídos com sucesso!")
 
 if __name__ == "__main__":
